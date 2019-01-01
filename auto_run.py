0588e949e2fb7686e76feb14474f953f8f2ad481
Bai <bxfhwzx@163.com>
2019-01-01 20:54:47 +0800

    'update'

--- auto_run.py
+++ auto_run.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import os
+import random
+import pandas as pd
 from datetime import datetime, timedelta, timezone
+
+dd = pd.date_range('2019-01-01', '2019-01-05')
+date_list = [pd.Timestamp(x).strftime("%Y-%m-%d") for x in dd.values]
+for date in date_list:
+	hour, minu, sec = random.randint(9,24), random.randint(0,60), random.randint(0,60)
+	time = "{}T{}:{}:{}+08:00".format(date, str(hour), str(minu), str(sec))
+	number = random.randint(0,10)
+	os.system("echo {} >> README.md".format(time))
+	os.system("echo {} >> README.md".format(str(number)))
+	os.system("echo. >> README.md")
+	for i in range(number):
+		os.system("git add . && git commit -m 'update' --date={}".format(time))
+		os.system('git push')
+exit(0)
+
+
+
 
 # 删除最后一行
 os.system("sed -i '$d' README.md")
+
 
 # 添加日期
 utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
