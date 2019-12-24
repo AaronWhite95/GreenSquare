196917f2437f41a9f9a055ee2def4f40df250a81
Bai <bxfhwzx@163.com>
2019-12-24 13:43:05 +0800

    'update'

--- auto_run.py
+++ auto_run.py
@@ -9,13 +9,12 @@ dd = pd.date_range('2019-12-23', '2019-12-25')
 date_list = [pd.Timestamp(x).strftime("%Y-%m-%d") for x in dd.values]
 for date in date_list:
 	number = random.randint(0,10)
+	os.system("echo {} commit {} times >> README.md".format(str(number)))
 	hours = sorted([random.randint(8,23) for _ in range(number)])
 	for hour in hours:
 		minu, sec = random.randint(0,60), random.randint(0,60)
 		time = "{}T{}:{}:{}+08:00".format(date, str(hour), str(minu), str(sec))
 		os.system("echo {} >> README.md".format(time))
-		os.system("echo {} >> README.md".format(str(number)))
-		os.system("echo. >> README.md")
 
 		os.system("git add . && git commit -m 'update' --date={}".format(time))
 		os.system('git push')
