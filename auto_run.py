9ac1de1277e0c49e83edf287a818a206a2c8ac5e
Bai <bxfhwzx@163.com>
2019-12-25 11:29:40 +0800

    'update'

--- auto_run.py
+++ auto_run.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, timezone
 dd = pd.date_range('2019-12-23', '2019-12-25')
 date_list = [pd.Timestamp(x).strftime("%Y-%m-%d") for x in dd.values]
 for date in date_list:
-	number = random.randint(0,10)
+	number = random.randint(0,5)
 	os.system("echo {} commit {} times >> README.md".format(str(number)))
 	hours = sorted([random.randint(8,23) for _ in range(number)])
 	for hour in hours:
